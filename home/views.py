8c87b3be3087e837eb1e6a28c42e61a48d846eef
jaksoftwares <jaksoftwares05@gmail.com>
2025-04-01 10:50:07 +0300

    adjusted mobile responsiveness

--- home/views.py
+++ home/views.py
@@ -6,9 +6,9 @@ from ministries.models import Ministry
 
 def home_page(request):
     featured_sermons = Sermon.objects.filter(featured=True).order_by('-publish_date')[:3]  
-    upcoming_events = Event.upcoming_events()[:3]  # Fetch 3 upcoming events
-    recent_events = Event.recent_events()[:3]  # Fetch 3 recent events
-    past_events = Event.past_events()[:3]  # Fetch 3 past events
+    upcoming_events = Event.upcoming_events()[:3]  
+    recent_events = Event.recent_events()[:3]  
+    past_events = Event.past_events()[:3]  
     ministries = Ministry.objects.all()[:4]
     context = {
         'featured_sermons': featured_sermons,
